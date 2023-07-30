d16a117ed698947551b36ee3a436a92e2212f528
James Tsaggaris <james.tsaggaris@gmail.com>
2023-07-30 09:27:44 -0500

    edit event ui

--- public/js/app.js
+++ public/js/app.js
@@ -8462,7 +8462,8 @@ function getdayeventdata(item, currentdate, timestamp, leftindent, columnwidth){
 	}
 
 	let output = ''
- 	output = `<div class="popupbutton eventwrap ${itemclasses.join(' ')}" id="${item.id}" onmousedown="clickevent(event, ${timestamp})" style="top:${mytop}px;height:${myheight}px;left:${leftindent / columnwidth * 100}%;width:${100 / columnwidth}%">
+ 	/*
+	output = `<div class="popupbutton eventwrap ${itemclasses.join(' ')}" id="${item.id}" onmousedown="clickevent(event, ${timestamp})" style="top:${mytop}px;height:${myheight}px;left:${leftindent / columnwidth * 100}%;width:${100 / columnwidth}%">
 		${!Calendar.Event.isReadOnly(item) ? itemclicks.join('') : ''}
 		<div class="${item.type == 1 ? `eventleftsmart` : `eventleft`} ${BORDERCOLORLIST[item.color]}"></div>
 		<div class="eventtext">
@@ -8483,6 +8484,28 @@ function getdayeventdata(item, currentdate, timestamp, leftindent, columnwidth){
 			
 		</div>
 		${item.type == 1 ? `<div class="eventleftsmart ${BORDERCOLORLIST[item.color]}"></div>` : ``}
+	</div>`
+	*/
+
+	output = `<div class="popupbutton eventwrap ${itemclasses.join(' ')}" id="${item.id}" onmousedown="clickevent(event, ${timestamp})" style="top:${mytop}px;height:${myheight}px;left:${leftindent / columnwidth * 100}%;width:${100 / columnwidth}%">
+		${!Calendar.Event.isReadOnly(item) ? itemclicks.join('') : ''}
+		<div class="eventtext">
+			<div class="eventtextspace"></div>
+			<div class="eventtextdisplay ${itemclasses2.join(' ')}">
+	
+				<span class="text-bold">${item.title ? cleanInput(item.title) : 'New Event'}</span>
+		
+				${item.type == 1 && item.priority != 0 ?
+					`<span class="todoitemcheckbox tooltip">
+						${getpriorityicon(item.priority)}
+					</span>`
+					:
+					''
+				}
+		
+				${myheight < 45 ? ' ' : '</br>'}${Calendar.Event.getShortStartEndText(item)}</div>
+			
+		</div>
 	</div>`
 
   return output
