2fdcc5f48ca54ce3581c55b1c8595ddeb018d693
James Tsaggaris <james.tsaggaris@gmail.com>
2023-07-30 20:07:37 -0700

    edit to do list

--- public/js/app.js
+++ public/js/app.js
@@ -298,6 +298,26 @@ function getcheckbox(boolean, tooltip){
 	}
 }
 
+
+//get big checkbox
+function getbigcheckbox(boolean, tooltip){
+	if(boolean){
+		return `<svg height="100%" stroke-miterlimit="10" style="fill-rule:nonzero;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;" viewBox="0 0 256 256" width="100%" class="buttoninlinelarge checkboxfilled">
+			<g>
+			<path d="M50 0C22.3858 0 0 22.3858 0 50L0 206C0 233.614 22.3858 256 50 256L206 256C233.614 256 256 233.614 256 206L256 50C256 22.3858 233.614 0 206 0L50 0ZM200.688 54.125C204.511 53.7858 208.449 54.9038 211.625 57.5625C217.978 62.8798 218.817 72.3349 213.5 78.6875L114.844 196.562C108.848 203.725 97.8393 203.725 91.8438 196.562L42.5 137.625C37.1827 131.272 38.0224 121.817 44.375 116.5C50.7276 111.183 60.1827 112.022 65.5 118.375L103.344 163.562L190.5 59.4375C193.159 56.2612 196.864 54.4642 200.688 54.125Z" fill-rule="nonzero" opacity="1" stroke="none"/>
+			</g>
+			</svg>
+	 		${tooltip || ''}`
+	}else{
+		return `<svg height="100%" stroke-miterlimit="10" style="fill-rule:nonzero;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;" viewBox="0 0 256 256" width="100%" class="buttoninlinelarge checkboxunfilled">
+			<g>
+			<path d="M50 10L206 10C228.091 10 246 27.9086 246 50L246 206C246 228.091 228.091 246 206 246L50 246C27.9086 246 10 228.091 10 206L10 50C10 27.9086 27.9086 10 50 10Z" fill="none" opacity="1" stroke-linecap="butt" stroke-linejoin="round" stroke-width="20"/>
+			</g>
+			</svg>
+	 		${tooltip || ''}`
+	}
+}
+
 //get check circle
 function getcheckcircle(boolean, tooltip){
 	if(boolean){
@@ -7124,10 +7144,12 @@ function gettododata(item){
 
 		 		<div class="todoitemcontainer padding-top-12px padding-bottom-12px margin-left-12px margin-right-12px relative">
 		 
+						${!schedulemytasksenabled ? `
 						<div class="display-flex flex-row gap-12px">
 							<div class="todoitemcheckbox tooltip display-flex" onclick="todocompleted(event, '${item.id}');gtag('event', 'button_click', { useraction: '${item.completed ? 'Mark uncomplete - task' : 'Mark complete - task'}' })">
 							${getcheckcircle(item.completed, item.completed ? '<span class="tooltiptextright">Mark uncomplete</span>' : '<span class="tooltiptextright">Mark complete</span>')}
 						</div>
+						` : ''}
 		
 						<div class="justify-flex-end flex-1 display-flex flex-row small:flex-column gap-12px">
 	
@@ -7205,14 +7227,9 @@ function gettododata(item){
 				</div>
 
 
-				${schedulemytasksenabled && schedulemytaskslist.find(g => g == item.id) ? 
-					`<div class="absolute todoitemselectcheck pointer pointer-auto background-blue padding-6px border-round">
-						<svg height="100%" stroke-miterlimit="10" style="fill-rule:nonzero;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;" viewBox="0 0 256 256" width="100%" class="buttoninline">
-							<g>
-							<path d="M93.2369 211.648L10 120.493" opacity="1" stroke-linecap="round" stroke-linejoin="miter" stroke-width="20"></path>
-							<path d="M93.2369 211.648L246 44.3518" opacity="1" stroke-linecap="round" stroke-linejoin="miter" stroke-width="20"></path>
-							</g>
-						</svg>
+				${schedulemytasksenabled ? 
+					`<div class="absolute todoitemselectcheck pointer pointer-auto">
+						${getbigcheckbox(schedulemytaskslist.find(g => g == item.id))}
 					</div>`
 				: ''}
 		 	</div>`
@@ -7221,7 +7238,7 @@ function gettododata(item){
   return output
 }
 
-
+//here4
 
 /*
 	functions for opening popup:
