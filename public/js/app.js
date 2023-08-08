9df8a0ce789b5a346555554da31bc074e462f635
James Tsaggaris <james.tsaggaris@gmail.com>
2023-08-08 14:03:22 -0700

    edit todo list

--- public/js/app.js
+++ public/js/app.js
@@ -7070,23 +7070,25 @@ function gettododata(item) {
 				
 								<div class="display-flex flex-wrap-wrap flex-row align-center column-gap-12px row-gap-6px">
 				 
+									<div class="gap-6px display-flex flex-row align-center width-fit todoitemtext nowrap pointer-none popupbutton" onclick="clicktodoitemduedate(event, '${item.id}')">
+										<div class="pointer-auto pointer ${!endbeforedate ? 'text-quaternary hoverunderlinequaternary' : (isoverdue ? 'text-red hoverunderlinered' : 'text-blue hoverunderlineblue')} text-14px ${itemclasses.join(' ')}">${endbeforedate ? `Due ${getHMText(item.endbefore.minute)}` : 'No due date'}</div>
+									</div>
+									
 									${Calendar.Event.isEvent(item) ? 
-										`<div class="gap-6px todoitemtextbutton display-flex flex-row align-center width-fit todoitemtext nowrap pointer-none popupbutton">
-											<div class="pointer-auto pointer text-green">Starts ${getHMText(item.start.minute)}</div>
+										`<div class="gap-6px display-flex flex-row align-center width-fit todoitemtext nowrap pointer-none popupbutton">
+											<div class="text-green">Scheduled for ${getHMText(item.start.minute)}</div>
 										</div>`
 										:
-										`<div class="gap-6px todoitemtextbutton display-flex flex-row align-center width-fit todoitemtext nowrap pointer-none popupbutton" onclick="clicktodoitemduedate(event, '${item.id}')">
-											<div class="pointer-auto pointer ${!endbeforedate ? 'text-quaternary hoverunderlinequaternary' : (isoverdue ? 'text-red hoverunderlinered' : 'text-blue hoverunderlineblue')} text-14px ${itemclasses.join(' ')}">${endbeforedate ? `Due ${getHMText(item.endbefore.minute)}` : 'No due date'}</div>
-										</div>`
+										``
 									}
 		
 									${Calendar.Event.isEvent(item) ? `` : 
-										`<div class="todoitemtextbutton width-fit todoitemtext nowrap text-14px pointer-auto pointer transition-duration-100 text-green hoverunderlinegreen pointer-auto pointer transition-duration-100 popupbutton ${itemclasses.join(' ')}" onclick="clicktodoitemduration(event, '${item.id}')">
+										`<div class="width-fit todoitemtext nowrap text-14px pointer-auto pointer transition-duration-100 text-green hoverunderlinegreen pointer-auto pointer transition-duration-100 popupbutton ${itemclasses.join(' ')}" onclick="clicktodoitemduration(event, '${item.id}')">
 											Takes ${getDHMText(myduration)}
 										</div>`
 									}
 	
-									<div class="todoitemtextbutton text-14px nowrap pointer-auto pointer popupbutton transition-duration-100 ${['text-quaternary hoverunderlinequaternary visibility-hidden hoverpriority small:visibility-visible', 'text-orange hoverunderlineorange', 'text-red hoverunderlinered'][item.priority]} ${itemclasses.join(' ')}" onclick="clicktodoitempriority(event, '${item.id}')">
+									<div class="text-14px nowrap pointer-auto pointer popupbutton transition-duration-100 ${['text-quaternary hoverunderlinequaternary visibility-hidden hoverpriority small:visibility-visible', 'text-orange hoverunderlineorange', 'text-red hoverunderlinered'][item.priority]} ${itemclasses.join(' ')}" onclick="clicktodoitempriority(event, '${item.id}')">
 										${['Low', 'Medium', 'High'][item.priority]} priority
 									</div>
 	
