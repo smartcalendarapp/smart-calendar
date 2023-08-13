009c2002b41824f3a1fb56587b964bb09fe219fd
James Tsaggaris <james.tsaggaris@gmail.com>
2023-08-13 07:53:59 -0700

    check for bug log

--- server.js
+++ server.js
@@ -1373,8 +1373,10 @@ app.post('/getclientgooglecalendar', async (req, res, next) => {
 		}
 
 		if(!req.session.tokens || !req.session.tokens.access_token){
+			console.warn('RIP HERE 1736')
 			let accesstoken = await getNewAccessToken(user.accountdata.refreshtoken)
 			if(!accesstoken){
+				console.warn('RIP HERE 1739')
 				return res.status(401).json({ error: 'Google login is expired, please <span onclick="connectgoogle()" class="pointer text-blue text-decoration-none hover:text-decoration-underline">log in with Google</span>.' })
 			}
 			req.session.tokens = req.session.tokens || {}
@@ -1416,6 +1418,7 @@ app.post('/getclientgooglecalendar', async (req, res, next) => {
 		
 		return res.json({ data: googlecalendardata })
 	}catch(error){
+		console.warn('RIP HERE 1420')
 		console.error(error)
 	  return res.status(401).json({ error: `Cannot access your Google Calendar, try to <span onclick="connectgoogle()" class="pointer text-blue text-decoration-none hover:text-decoration-underline">log in with Google</span> again.` })
 	}
