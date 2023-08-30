e79b8a94084bc1d0c12ef6a328106dacd23de17c
James Tsaggaris <james.tsaggaris@gmail.com>
2023-08-30 08:07:30 -0700

    edit theme flicker

--- public/js/app.js
+++ public/js/app.js
@@ -4079,6 +4079,10 @@ if ('matchMedia' in window) {
 
 settheme(getStorage('theme') || devicetheme)
 
+setTimeout(function(){
+	document.documentElement.classList.remove('disabletransition')
+},300)
+
 function changedevicetheme(event) {
 	if (event.matches) {
 		devicetheme = 'dark'
