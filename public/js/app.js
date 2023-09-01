849db15d3797bc9f8c2f4fbe31344dd8ca6a3627
James Tsaggaris <james.tsaggaris@gmail.com>
2023-08-31 21:34:31 -0700

    edit AS

--- public/js/app.js
+++ public/js/app.js
@@ -9926,7 +9926,7 @@ async function autoScheduleV2(smartevents, addedtodos, resolvedpassedtodos) {
 			let tempenddate2 = new Date(item2.end.year, item2.end.month, item2.end.day, 0, item2.end.minute)
 
 			let spacing = getbreaktime(item2)
-			console.log(tempstartdate1 + '', tempstartdate2 + '', spacing)
+			console.log(tempstartdate1 + '', tempstartdate2 + '', spacing, tempstartdate1.getTime() < tempenddate2.getTime() + spacing && tempenddate1.getTime() + spacing > tempstartdate2.getTime())
 
 			if (tempstartdate1.getTime() < tempenddate2.getTime() + spacing && tempenddate1.getTime() + spacing > tempstartdate2.getTime()) {
 				return [item2, spacing]
