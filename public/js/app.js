e67900a9bad620e5381242995f449e7ec4196565
James Tsaggaris <james.tsaggaris@gmail.com>
2023-09-24 16:08:49 -0700

    edit

--- public/js/app.js
+++ public/js/app.js
@@ -2308,6 +2308,13 @@ class Calendar {
 			schedulemytasksactive.classList.add('display-none')
 		}
 
+		let plantasksselectall = getElement('plantasksselectall')
+		if(schedulemytaskslist.length > 0){
+			plantasksselectall.innerHTML = 'Deselect all'
+		}else{
+			plantasksselectall.innerHTML = 'Select all'
+		}
+
 		let plantaskssubmit = getElement('plantaskssubmit')
 		schedulemytaskslist = schedulemytaskslist.filter(d => calendar.todos.find(g => g.id == d))
 		plantaskssubmit.innerHTML = `<div class="display-flex flex-row gap-6px align-center background-blue hover:background-blue-hover padding-8px-12px border-round transition-duration-100 pointer ${schedulemytaskslist.length > 0 ? '' : 'greyedoutevent'}" ${schedulemytaskslist.length > 0 ? `onclick="submitschedulemytasks()"` : ''}>
@@ -3003,6 +3010,12 @@ class Calendar {
 		enablediscordnotif2.checked = calendar.discordreminderenabled
 
 
+		//discord notif
+		let connectdiscordstatus2 = getElement('connectdiscordstatus2')
+		let connectdiscordstatus = getElement('connectdiscordstatus')
+		connectdiscordstatus.classList.remove('display-none')
+		connectdiscordstatus2.classList.remove('display-none')
+
 		let discordtext;
 		if(clientinfo.discord.id && calendar.discordreminderenabled){
 			discordtext = `<svg height="100%" stroke-miterlimit="10" style="fill-rule:nonzero;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;" viewBox="0 0 256 256" width="100%" class="buttoninline checkboxfilledgreen">
@@ -3012,18 +3025,17 @@ class Calendar {
 				</g>
 			</svg>
 			<div class="text-14px text-green">Connected</div>`
-		}else{
+		}else if(!calendar.discordreminderenabled){
 			discordtext = `<div class="width-fit display-flex flex-row gap-6px align-center border-8px nowrap flex-1 discordbutton text-center text-14px padding-8px-12px pointer transition-duration-100" onclick="logindiscord()">
 				<svg width="16" height="16" class="fillbuttonwhite" viewBox="0 0 16 16">
 					<path d="M13.545 2.907a13.227 13.227 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.19 12.19 0 0 0-3.658 0 8.258 8.258 0 0 0-.412-.833.051.051 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.041.041 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032c.001.014.01.028.021.037a13.276 13.276 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019c.308-.42.582-.863.818-1.329a.05.05 0 0 0-.01-.059.051.051 0 0 0-.018-.011 8.875 8.875 0 0 1-1.248-.595.05.05 0 0 1-.02-.066.051.051 0 0 1 .015-.019c.084-.063.168-.129.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.052.052 0 0 1 .053.007c.08.066.164.132.248.195a.051.051 0 0 1-.004.085 8.254 8.254 0 0 1-1.249.594.05.05 0 0 0-.03.03.052.052 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.235 13.235 0 0 0 4.001-2.02.049.049 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.034.034 0 0 0-.02-.019Zm-8.198 7.307c-.789 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612Zm5.316 0c-.788 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612Z"></path>
 				</svg>
 				Connect to Discord
 			</div>`
+		}else{
+			connectdiscordstatus.classList.add('display-none')
+			connectdiscordstatus2.classList.add('display-none')
 		}
-
-
-		let connectdiscordstatus2 = getElement('connectdiscordstatus2')
-		let connectdiscordstatus = getElement('connectdiscordstatus')
 
 		connectdiscordstatus.innerHTML = discordtext
 		connectdiscordstatus2.innerHTML = discordtext
@@ -5642,7 +5654,7 @@ async function disconnectgoogle() {
 
 	if (response.status == 200) {
 		await getclientinfo()
-		
+
 		calendar.settings.issyncingtogooglecalendar = false
 		calendar.settings.issyncingtogoogleclassroom = false
 		calendar.updateSettings()
@@ -6928,6 +6940,14 @@ function clickschedulemytasks(event) {
 function closeschedulemytasks() {
 	schedulemytasksenabled = false
 	calendar.updateTodo()
+}
+
+function selectallschedulemytasks(){
+	if(schedulemytaskslist.length > 0){
+		schedulemytaskslist = []
+	}else{
+		schedulemytaskslist = [...calendar.todos.filter(item => Calendar.Todo.isSchedulable(item) && (item.endbefore.year != null && item.endbefore.month != null && item.endbefore.day != null && item.endbefore.minute != null)).map(d => d.id)]
+	}
 }
 
 //select task
