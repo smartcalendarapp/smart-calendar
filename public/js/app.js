87a8268d8abedcc67865fd88ae1e16625a6964b1
James Tsaggaris <james.tsaggaris@gmail.com>
2023-09-24 09:03:34 -0700

    edit run

--- public/js/app.js
+++ public/js/app.js
@@ -3005,7 +3005,7 @@ class Calendar {
 
 		let discordtext;
 		if(!clientinfo.discord.id || !calendar.discordreminderenabled){
-			discordtext = `<div class="width-fit display-flex flex-row gap-6px align-center border-8px nowrap flex-1 discordbutton text-center text-14px padding-8px-12px pointer transition-duration-100 display-none" onclick="logindiscord()">
+			discordtext = `<div class="width-fit display-flex flex-row gap-6px align-center border-8px nowrap flex-1 discordbutton text-center text-14px padding-8px-12px pointer transition-duration-100" onclick="logindiscord()">
 				<svg width="16" height="16" class="fillbuttonwhite" viewBox="0 0 16 16">
 					<path d="M13.545 2.907a13.227 13.227 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.19 12.19 0 0 0-3.658 0 8.258 8.258 0 0 0-.412-.833.051.051 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.041.041 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032c.001.014.01.028.021.037a13.276 13.276 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019c.308-.42.582-.863.818-1.329a.05.05 0 0 0-.01-.059.051.051 0 0 0-.018-.011 8.875 8.875 0 0 1-1.248-.595.05.05 0 0 1-.02-.066.051.051 0 0 1 .015-.019c.084-.063.168-.129.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.052.052 0 0 1 .053.007c.08.066.164.132.248.195a.051.051 0 0 1-.004.085 8.254 8.254 0 0 1-1.249.594.05.05 0 0 0-.03.03.052.052 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.235 13.235 0 0 0 4.001-2.02.049.049 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.034.034 0 0 0-.02-.019Zm-8.198 7.307c-.789 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612Zm5.316 0c-.788 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612Z"></path>
 				</svg>
@@ -3536,21 +3536,6 @@ let lastupdatecalendardate = 0
 function run() {
 	//ONCE
 
-	//push notif
-	window.addEventListener('mousedown', clickforpushnotif, false)
-	window.addEventListener('touchstart', clickforpushnotif, false)
-
-	function clickforpushnotif(event){
-		if (calendar.pushSubscriptionEnabled) {
-			enablePushNotifs()
-		} else {
-			removePushNotifs()
-		}
-		
-		window.removeEventListener('mousedown', clickforpushnotif, false)
-		window.removeEventListener('touchstart', clickforpushnotif, false)
-	}
-
 	//theme
 	updatetheme()
 
@@ -3577,15 +3562,17 @@ function run() {
 
 	//scroll Y
 	let currentdate = new Date()
-	scrollcalendarY(currentdate.getHours() * 60 + currentdate.getMinutes())
+	setTimeout(function(){
+		scrollcalendarY(currentdate.getHours() * 60 + currentdate.getMinutes())
+	}, 1000)
 
 
 	//welcome popup
-	setTimeout(function () {
-		if (calendar.welcomepopup.calendar == false && calendar.onboarding.addtask == true) {
-			//getElement(welcomepopupdata.calendar).classList.remove('hiddenfade')
-		}
-	}, 10000)
+	if (calendar.welcomepopup.calendar == false && calendar.onboarding.addtask == true ) {
+		setTimeout(function () {
+			getElement(welcomepopupdata.calendar).classList.remove('hiddenfade')
+		}, 10000)
+	}
 
 
 
@@ -3680,6 +3667,22 @@ function run() {
 			await getclientdata()
 		}
 	}, 3000)
+
+
+	//push notif
+	window.addEventListener('mousedown', clickforpushnotif, false)
+	window.addEventListener('touchstart', clickforpushnotif, false)
+
+	function clickforpushnotif(event){
+		if (calendar.pushSubscriptionEnabled) {
+			enablePushNotifs()
+		} else {
+			removePushNotifs()
+		}
+		
+		window.removeEventListener('mousedown', clickforpushnotif, false)
+		window.removeEventListener('touchstart', clickforpushnotif, false)
+	}
 
 }
 
@@ -4751,6 +4754,8 @@ function press2(event){
 	if(event.key == 'e'){
 		window.removeEventListener('keydown', press2, false)
 		window.addEventListener('keydown', press3, false)
+	}else{
+		window.removeEventListener('keydown', press2, false)
 	}
 }
 function press3(event){
@@ -4760,6 +4765,8 @@ function press3(event){
 		if(clientinfo.google_email == 'james.tsaggaris@gmail.com'){
 			getElement('devpopup').classList.remove('hiddenpopup')
 		}
+	}else{
+		window.removeEventListener('keydown', press3, false)
 	}
 }
 
