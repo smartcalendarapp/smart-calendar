312d6560b4e9ecb74f4b1303b8089c5e19e2303b
James Tsaggaris <james.tsaggaris@gmail.com>
2023-10-03 08:17:01 -0700

    edit gront

--- public/js/home.js
+++ public/js/home.js
@@ -238,10 +238,10 @@ function updatescroll(event){
 
 	if(rectfirst.bottom < windowheight && rectlast.top > windowheight){
 		getElement('backgroundeffect').classList.remove('hiddenfadeslow')
-		getElement('backgroundeffectnavbar').classList.remove('hiddenfade')
+		getElement('backgroundeffectnavbar').classList.remove('hiddenfadeslow')
 	}else{
 		getElement('backgroundeffect').classList.add('hiddenfadeslow')
-		getElement('backgroundeffectnavbar').classList.add('hiddenfade')
+		getElement('backgroundeffectnavbar').classList.add('hiddenfadeslow')
 	}
 
 
