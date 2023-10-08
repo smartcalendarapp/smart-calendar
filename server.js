4cefa1416ed07a7b9368d0ebf2abbe0111fb5fe4
James Tsaggaris <james.tsaggaris@gmail.com>
2023-10-07 20:48:46 -0700

    edit

--- server.js
+++ server.js
@@ -1025,7 +1025,7 @@ app.use((req, res, next) => {
 //GOOGLE ROUTES
 
 app.post('/auth/google', async (req, res, next) => {
-	console.warn(req.headers)
+	console.warn(req.query)
 	try{
 		let options = req.body.options
 
