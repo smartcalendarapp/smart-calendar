385f0d35b62b1586fddd58c3904c0d17c25c0903
James Tsaggaris <james.tsaggaris@gmail.com>
2023-10-14 20:54:14 -0700

    edit apple auth

--- server.js
+++ server.js
@@ -1412,8 +1412,6 @@ app.post('/auth/apple/callback', async (req, res) => {
 		if (!code) {
 			throw new Error('Missing code parameter')
 		}
-
-		const privateKey = fs.readFileSync(APPLE_PRIVATE_KEY_PATH)
   
 		const clientsecret = jwt.sign({
 			iss: APPLE_TEAM_ID,
@@ -1440,12 +1438,8 @@ app.post('/auth/apple/callback', async (req, res) => {
 		})
 	
 		const tokenData = await tokenResponse.json()
-	
-		// Validate access token
-		const applePublicKey = await fetch('https://appleid.apple.com/auth/keys')
-		const appleKeys = await applePublicKey.json()
-	
-		const decodedToken = jwt.verify(tokenData.id_token, appleKeys.keys[0].n, {
+		
+		const decodedToken = jwt.verify(tokenData.id_token, publicKey, {
 			algorithms: ['RS256'],
 			audience: APPLE_CLIENT_ID
 		})
