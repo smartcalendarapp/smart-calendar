a66c6262aee8eb5b859725a2e23d46668830316f
James Tsaggaris <james.tsaggaris@gmail.com>
2023-11-28 17:20:02 -0800

    blog rearrange

--- server.js
+++ server.js
@@ -1052,6 +1052,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'public', 'css')))
 app.use(express.static(path.join(__dirname, 'public', 'js')))
 app.use(express.static(path.join(__dirname, 'public', 'images')))
+app.use(express.static(path.join(__dirname, 'public', 'blog')))
 
 app.use((req, res, next) => {
   if (req.path.endsWith('.html')) {
