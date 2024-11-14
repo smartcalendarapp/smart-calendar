5d3f6f103e8ff6e64bfbe7d87ce59611a3ed755a
James Tsaggaris <james.tsaggaris@gmail.com>
2024-11-14 15:29:42 -0800

    edot

--- public/js/memgrow.js
+++ public/js/memgrow.js
@@ -1,3 +1,14 @@
+(function() {
+
+    if (!window.navigator) window.navigator = {};
+
+    window.navigator.getUserMedia = function() {
+
+        webkit.messageHandlers.callbackHandler.postMessage(arguments);
+
+    }
+
+})();
 
 function getelement(id){
     return document.getElementById(id)
