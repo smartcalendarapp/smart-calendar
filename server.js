afdbd36f4fed6087e4ced00204b0844991f8589d
James Tsaggaris <james.tsaggaris@gmail.com>
2024-11-16 09:54:25 -0800

    edit

--- server.js
+++ server.js
@@ -7017,6 +7017,7 @@ const CardExtraction = z.object({
 app.post('/saveuserdata', async (req, res) => {
     try{
 		if(req?.body?.secretToken != process.env.MEMGROW_SECRET && req?.session?.user?.userid != DEV_ID) return res.status(401).end()
+			console.log(JSON.stringify(req.body.daata))
 
         let data = req.body.data
 		let lastedited = req.body.lastedited
@@ -7046,7 +7047,6 @@ app.post('/getuserdatalastedited', async (req, res) => {
 
 app.post('/getuserdata', async (req, res) => {
     try{
-		console.warn(req?.body)
 		if(req?.body?.secretToken != process.env.MEMGROW_SECRET && req?.session?.user?.userid != DEV_ID) return res.status(401).end()
 
         const data = await getmemgrowdata(DEV_ID)
