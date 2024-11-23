07a0d278402647b05a7088c670e181621f9ad2b9
James Tsaggaris <james.tsaggaris@gmail.com>
2024-11-22 22:53:52 -0800

    edit

--- server.js
+++ server.js
@@ -7092,7 +7092,7 @@ app.post('/getcardhint', async (req, res) => {
 	const card = req.body.card
 	const showanswer = req.body.showanswer
 
-	const systemprompt = `User needs help memorizing, use memory loci and palace. Make it easy to read and visualize.${showanswer ? '' : ` Don't give away the card back.`} 2-3 sentences. If studying a language, help user remember the pronounciation.`
+	const systemprompt = `User needs help memorizing, use memory loci and palace, analogies, and/or word play. Make it easy to read and visualize.${showanswer ? '' : ` Don't give away the card back.`} 2-3 sentences. If studying a language, help user remember the pronounciation.`
 
 	const userprompt = `Card front: """${card.fronttext}"""\nCard back: """${card.backtext}"""`
 
